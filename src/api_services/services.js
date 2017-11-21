b435c1175576e543fd54805b9a80947bb8c82fba
Server <developer@cirtru.com>
2017-11-22 00:04:26 +0530

    Fixing eslint errors

--- src/api_services/services.js
+++ src/api_services/services.js
@@ -1,6 +1,10 @@
 const PUMPUP_ENDPOINT_PREFIX = 'http://api.pumpup.com'
 
 class PumpupService {
+
+  /**
+   * get User's Bio from the server
+   */
   async getUserBio() {
     const url = PUMPUP_ENDPOINT_PREFIX + "/1/classes/User/318381"
     const response = await fetch(url, {
@@ -27,6 +31,11 @@ class PumpupService {
 
   }
 
+
+
+  /**
+   * get user feed images from the server
+   */
   async getUserFeedImages() {
     const url = PUMPUP_ENDPOINT_PREFIX + "/1/functions/feed/profile/load-batch"
     const response = await fetch(url, {
@@ -56,33 +65,36 @@ class PumpupService {
   }
 
 
+
+  /**
+   * get popular Feed Images from the server
+   */
   async getPopularFeedImages() {
-  const url = PUMPUP_ENDPOINT_PREFIX + "/1/functions/feed/popular/load-batch"
-  response = await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      "isThumbnailsOnly": true,
-      "limit": 18,
-      "_method": "POST",
-      "_version": "5.0.5",
-      "_SessionToken": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOjI3MDc3OTgsImV4cCI6MTUzOTUzNTI1OTM2OH0.UK2qP1yk9QLk_Bkx1Ly0RPaitRYtec8ojZhzYRc0D-g"
+    const url = PUMPUP_ENDPOINT_PREFIX + "/1/functions/feed/popular/load-batch"
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          "isThumbnailsOnly": true,
+          "limit": 18,
+          "_method": "POST",
+          "_version": "5.0.5",
+          "_SessionToken": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOjI3MDc3OTgsImV4cCI6MTUzOTUzNTI1OTM2OH0.UK2qP1yk9QLk_Bkx1Ly0RPaitRYtec8ojZhzYRc0D-g"
+        })
     })
-  })
 
-  if(!response.ok) {
-    throw new Error("PumpupService getPopularFeedImages failed, HTTP status " + response.status)
+    if(!response.ok) {
+      throw new Error('PumpupService getPopularFeedImages failed, HTTP status ' + response.status)
+    }
+
+    const data = await response.json()
+
+    return data
+
   }
-
-  const data = await response.json()
-
-  return data
-
-}
-
 }
 
 export default new PumpupService()
--- src/util.js
+++ src/util.js
@@ -6,14 +6,16 @@ export const getParsedBio = function(bioString, hashTagTarget) {
   //exclude carriage returns
   const str = bioString.replace(/\r?\n|\r/g, ' ')
 
-  let strArr = str.split(" ")
+  let strArr = str.split(' ')
   const newArr = []
   strArr.forEach((str, i) => {
     let strComp
-    if(str[0] === "#") {
-      strComp = <Text key={`word${i}`} style={{color: 'blue'}} onPress={() => Linking.openURL(hashTagTarget)}>{str} </Text>
-    } else if(str[0] === "@") {
-      strComp = <Text key={`word${i}`} style={{color: 'blue'}} onPress={() => Linking.openURL("https://twitter.com/PumpUp")}>{str} </Text>
+    if(str[0] === '#') {
+      strComp = <Text key={`word${i}`} style={{color: 'blue'}} onPress={() =>
+        Linking.openURL(hashTagTarget)}>{str} </Text>
+    } else if(str[0] === '@') {
+      strComp = <Text key={`word${i}`} style={{color: 'blue'}} onPress={() =>
+        Linking.openURL('https://twitter.com/PumpUp')}>{str} </Text>
     } else {
       strComp = <Text key={`word${i}`}>{str} </Text>
     }

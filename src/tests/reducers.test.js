18ac67e91924691c33e140fefce31462f5cedb8c
Server <developer@cirtru.com>
2017-11-22 00:22:47 +0530

    Fixing linting errors

--- src/tests/reducers.test.js
+++ src/tests/reducers.test.js
@@ -1,48 +1,36 @@
-import React from 'react'
-import {should} from 'should/should.js'
-import {userDetails} from '../redux'
-
-const initialState = {
-  userDetails: {
-    user: {},
-
-    userFeedImages: [],
-
-    popularFeedImages: [],
-  }
-}
+import { userDetails } from '../redux'
 
 const newState = {
   userDetails: {
     user: {
-      bio: "Hello this is my bio",
-      name: "Pumpup"
+      bio:  'Hello this is my bio',
+      name: 'Pumpup'
     },
 
     userFeedImages: [
-      {thumbnail: "https://abc.com/abc.jpg"},
-      {thumbnail: "https://abc.com/abc.jpg"},
-      {thumbnail: "https://abc.com/abc.jpg"},
-      {thumbnail: "https://abc.com/abc.jpg"},
-      {thumbnail: "https://abc.com/abc.jpg"},
-      {thumbnail: "https://abc.com/abc.jpg"},
+      {thumbnail : 'https://abc.com/abc.jpg'},
+      {thumbnail : 'https://abc.com/abc.jpg'},
+      {thumbnail : 'https://abc.com/abc.jpg'},
+      {thumbnail : 'https://abc.com/abc.jpg'},
+      {thumbnail : 'https://abc.com/abc.jpg'},
+      {thumbnail : 'https://abc.com/abc.jpg'},
     ],
 
     popularFeedImages: [
-      {thumbnail: "https://abc.com/abc.jpg"},
-      {thumbnail: "https://abc.com/abc.jpg"},
-      {thumbnail: "https://abc.com/abc.jpg"},
+      {thumbnail : 'https://abc.com/abc.jpg'},
+      {thumbnail : 'https://abc.com/abc.jpg'},
+      {thumbnail : 'https://abc.com/abc.jpg'},
     ],
   }
 }
 
 const errorState = {
   userDetails: {
-    user: { error: true },
+    user              : { error: true },
 
-    userFeedImages: { error: true },
+    userFeedImages    : { error: true },
 
-    popularFeedImages: { error: true },
+    popularFeedImages : { error: true },
   }
 }
 describe('tests for reducers', function(){
@@ -50,17 +38,18 @@ describe('tests for reducers', function(){
 
     const init_state = userDetails().userDetails
 
-    init_state.user.should.be.an.Object().and.be.empty();
+    init_state.user.should.be.an.Object().and.be.empty()
 
-    init_state.userFeedImages.should.be.instanceof(Array).and.have.lengthOf(0);
+    init_state.userFeedImages.should.be.instanceof(Array).and.have.lengthOf(0)
 
-    init_state.popularFeedImages.should.be.instanceof(Array).and.have.lengthOf(0);
+    init_state.popularFeedImages.should.be.instanceof(Array).and.have.lengthOf(0)
 
   })
 
   it('reducer should return correct state on success of fetching user', function() {
 
-    const new_user = userDetails(newState, {type: 'SUCCESS_FETCH_USER'}).userDetails.user
+    const new_user = userDetails(newState,
+      {type: 'SUCCESS_FETCH_USER'}).userDetails.user
 
     new_user.should.have.property('name', 'Pumpup')
     new_user.should.have.property('bio', 'Hello this is my bio')
@@ -69,7 +58,8 @@ describe('tests for reducers', function(){
 
   it('reducer should return error state on failure of fetching user', function() {
 
-    const new_user = userDetails(errorState, {type: 'ERROR_FETCH_USER'}).userDetails.user
+    const new_user = userDetails(errorState,
+      {type: 'ERROR_FETCH_USER'}).userDetails.user
 
     new_user.should.have.property('error', true)
 
@@ -77,15 +67,17 @@ describe('tests for reducers', function(){
 
   it('reducer should return correct user feed images on success of fetching user feed images', function() {
 
-    const userFeedImages = userDetails(newState, {type: 'SUCCESS_FETCH_USER_FEED_IMAGES'}).userDetails.userFeedImages
+    const userFeedImages = userDetails(newState,
+      {type: 'SUCCESS_FETCH_USER_FEED_IMAGES'}).userDetails.userFeedImages
 
-    userFeedImages.should.be.instanceof(Array).and.have.lengthOf(6);
+    userFeedImages.should.be.instanceof(Array).and.have.lengthOf(6)
 
   })
 
   it('reducer should return error state on failure of fetching user feed images', function() {
 
-    const userFeedImages = userDetails(errorState, {type: 'ERROR_FETCH_USER_FEED_IMAGES'}).userDetails.userFeedImages
+    const userFeedImages = userDetails(errorState,
+      {type: 'ERROR_FETCH_USER_FEED_IMAGES'}).userDetails.userFeedImages
 
     userFeedImages.should.have.property('error', true)
 
@@ -94,7 +86,8 @@ describe('tests for reducers', function(){
 
   it('reducer should return correct popular feed images on success of fetching popular feed images', function() {
 
-    const popularFeedImages = userDetails(newState, {type: 'SUCCESS_FETCH_POPULAR_FEED_IMAGES'}).userDetails.popularFeedImages
+    const popularFeedImages = userDetails(newState,
+      {type: 'SUCCESS_FETCH_POPULAR_FEED_IMAGES'}).userDetails.popularFeedImages
 
     popularFeedImages.should.be.instanceof(Array).and.have.lengthOf(3)
 
@@ -102,7 +95,8 @@ describe('tests for reducers', function(){
 
   it('reducer should return error state on failure of fetching popular feed images', function() {
 
-    const popularFeedImages = userDetails(errorState, {type: 'ERROR_FETCH_POPULAR_FEED_IMAGES'}).userDetails.popularFeedImages
+    const popularFeedImages = userDetails(errorState,
+      {type: 'ERROR_FETCH_POPULAR_FEED_IMAGES'}).userDetails.popularFeedImages
 
     popularFeedImages.should.have.property('error', true)
 
